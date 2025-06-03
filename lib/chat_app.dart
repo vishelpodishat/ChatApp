d0b7d200dbb9f38f3a1422a4df2308e2ef9bfebe
Alisher Saideshov <90091472+vishelpodishat@users.noreply.github.com>
2025-06-03 12:13:29 +0500

    init

---
+++ lib/chat_app.dart
@@ -0,0 +1,15 @@
+import 'package:chat_app/home_page.dart';
+import 'package:flutter/material.dart';
+
+class ChatApp extends StatelessWidget {
+  const ChatApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)),
+      home: HomePage(),
+    );
+  }
+}
