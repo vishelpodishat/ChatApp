6e4e7b1e6438af189526b608c8e6eef145815803
Alisher Saideshov <90091472+vishelpodishat@users.noreply.github.com>
2025-06-06 23:27:51 +0500

    firebase init

--- lib/chat_app.dart
+++ lib/chat_app.dart
@@ -7,6 +7,7 @@ class ChatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)),
       home: HomePage(),
